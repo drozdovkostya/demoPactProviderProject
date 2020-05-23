c625205cb0d73059b3092b385a0b8fa989a93514
drozdov <konstantin.drozdov@diymaxeda.com>
2020-05-23 14:30:33 +0300

    Fixed test

--- src/main/java/com/example/demoPactProvider/service/UserService.java
+++ src/main/java/com/example/demoPactProvider/service/UserService.java
@@ -7,7 +7,7 @@ public class UserService {
 
         User user = new User();
         user.setAge(12);
-        user.setFirstName("Arnold");
+        user.setFirstName(firstName);
         user.setLastName("Johnson");
         user.setPosition("Admin");
         user.setPostalCode("1111");
